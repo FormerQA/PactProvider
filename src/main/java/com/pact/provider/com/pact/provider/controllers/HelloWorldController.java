d6632192776e22eeea14dc4dedb3f8c0d075f528
Pravitha Nair <pravitha.nair@target.com>
2018-09-04 11:05:32 +0530

    functional change

--- src/main/java/com/pact/provider/com/pact/provider/controllers/HelloWorldController.java
+++ src/main/java/com/pact/provider/com/pact/provider/controllers/HelloWorldController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
-    private static final String template = "Welcome %s!";
+    private static final String template = "Hello %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping
